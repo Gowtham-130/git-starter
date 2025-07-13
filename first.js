65755ed1b6fb7227e1109277f6c02e76940b0b4a
Gowtham-130 <gowtham13zeros@gmail.com>
2025-07-14 03:33:19 +0530

    Add files via upload
    these are first four session files on NM_Y

---
+++ first.js
@@ -0,0 +1,79 @@
+/*console.log("Hii")
+var a = 5;
+if (a < 5) {
+    console.log("a isa greater than 5")
+} else if (a == 5) {
+    console.log(" a is equal to 5")
+
+} else {
+    console.log("a is less than 5")
+}
+console.log(new Date().getMonth())
+
+switch (new Date().getMonth()) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("thuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("happy happy happy")
+        break;
+
+
+}
+
+var grade = "E"
+
+
+switch (grade) {
+    case "A":
+        console.log("OUtstanding");
+        break;
+    case "B":
+        console.log("good");
+        break;
+    case "C":
+
+        console.log("bad");
+        break;
+    case "D":
+        console.log("just pass");
+        break;
+    case "E":
+        console.log("fail");
+
+        break;
+}
+for (var a = 0; a <= 5; a++) {
+    console.log(a)
+}
+i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+i = 0
+do {
+    console.log(i);
+    i++;
+} while (i >= 10)*/
+for (var i = 1; i <= 10; i++) {
+    console.log("2*" + i + "=" + i * 2)
+}
